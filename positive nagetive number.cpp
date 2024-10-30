a0ba9bf654a0bb2bb78f2eec91f22e38355dd52f
MD FORIDUL IBNE QAUSER <foridulislam35962@gmail.com>
2024-10-30 18:56:04 +0600

    codeforce 2033A

---
+++ positive nagetive number.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,a;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a;
+        if(a%2==0)
+            cout<<"Sakurako"<<endl;
+        else
+            cout<<"Kosuke"<<endl;
+    }
+    return 0;
+}
