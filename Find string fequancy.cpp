18d188849b805090aa1f14f7926050b3e8672612
MD FORIDUL IBNE QAUSER <foridulislam35962@gmail.com>
2024-11-22 23:07:23 +0600

    Google Interview Problem

---
+++ Find string fequancy.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    int result=0;
+    vector<string>v;
+    cin>>t;
+    while(t--)
+    {
+        
+        int f;
+        cin>>f;
+        for(int i=0;i<f;i++)
+        {
+            string s;
+            cin>>s;
+            v.push_back(s);
+        }
+        map<string,int>cnt;
+        for(auto u:v)
+            cnt[u]++;
+        for(int i=0;i<v.size();i++)
+        {
+            if(cnt[v[i]]==2)
+                result++;
+        }
+        cout<<result<<endl;
+        v.clear();
+    }
+    return 0;
+}
