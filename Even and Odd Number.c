b58be6daadddc639c61865274d7d4ece4b0d89b1
MD FORIDUL IBNE QAUSER <foridulislam35962@gmail.com>
2025-03-18 17:04:12 +0600

    normal program

---
+++ Even and Odd Number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number= ");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("%d is a Even number",a);
+    }
+    else
+    {
+        printf("%d is a Odd number",a);
+    }
+    return 0;
+}
